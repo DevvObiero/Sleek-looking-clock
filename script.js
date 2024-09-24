6ea82766768d716ad8813c8e791eb3aad4e41c71
Obinazo <obieropauldev@gmail.com>
2024-09-24 17:42:00 +0300

    fixed i typo that almost drove me crazy

--- script.js
+++ script.js
@@ -15,3 +15,50 @@ const clock = () =>{
 }
 
 setInterval(clock, 1000);
+
+
+
+
+const textHour = document.getElementById('text-hour'),
+      textMinutes = document.getElementById('text-minutes'),
+      textAmPm = document.getElementById('text-ampm'),
+      dateDay = document.getElementById('date-day'),
+      dateMonth = document.getElementById('date-month'),
+      dateYear = document.getElementById('date-year')
+
+
+      const  clockText = () => {
+        let date = new Date(),
+        hh = date.getHours(),
+        mm = date.getMinutes(),
+        // ss = date.getSeconds(),
+        // ampm = hh >= 12? 'PM' : 'AM',
+        ampm,
+        day = date.getDate(),
+        month = date.getMonth(),
+        year = date.getFullYear()
+        // day = date.toLocaleString('default', {weekday: 'long'}),
+        // month = date.toLocaleString('default', {month: 'long'}),
+        // year = date.getFullYear()
+
+        if(hh >= 12){
+            hh = hh - 12;
+            ampm = 'PM'
+        }else {
+            ampm = 'AM'
+        }
+
+        if
+
+
+
+
+
+
+      
+        textHour.innerHTML = `${hh}`
+      }
+
+      setInterval(clockText,1000)//1s
+
+     
